2985f71a1f9ebb461e0cb05af82622d6f457297d
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2022-08-27 17:19:18 +0200

    Add graphic interface

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from pytube import YouTube
+from tkinter import *
+
+main = Tk()
+
+main.mainloop()
 
 print("Paste link here: ")
 link = input()
@@ -12,4 +17,8 @@ soundOnly = input()
 print("Set resolution: 144p, 240p, 360p, 480p, 720p, 1080p")
 resolution = input()
 
-stream = yt.streams.filter(only_audio = soundOnly, res = resolution).first().download('/Users/krusz/OneDrive/Pulpit/MusicFromYT')
+print("Set location path")
+path = input()
+
+stream = yt.streams.filter(only_audio = soundOnly, res = resolution).first().download(path)
+#stream = yt.streams.filter(only_audio = soundOnly, res = resolution).first().download('/Users/krusz/OneDrive/Pulpit/MusicFromYT')
