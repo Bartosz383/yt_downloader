77931e82f6ac028c33c17b0ca9632c6adaa38d92
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2022-08-27 15:01:32 +0200

    Cleaning code
    Removal of unnecessary lines of code

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from pytube import YouTube
 from sys import argv
 
-#link = argv[1]
-#yt = YouTube(link)
-
 print("Paste link here: ")
 link = input()
 
@@ -11,12 +8,6 @@ yt = YouTube(link)
 print("Title: ", yt.title)
 print("View: ", yt.views)
 
-#print("Title: '", yt.title)
-
-#print("View: ", yt.views)
-
 stream = yt.streams.first()
-#stream.download()
 
 stream.download('/Users/krusz/OneDrive/Pulpit/MusicFromYT')
-#yd.download('C:\Users\krusz\OneDrive\Pulpit\Nowy folder')
