3ac7e4333a20ea0afc29f4680de3064bf7504ffb
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2022-08-27 16:34:50 +0200

    Set resolution

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ print("Paste link here: ")
 link = input()
 
 yt = YouTube(link)
-print("Title: ", yt.title, "\n View: ", yt.views)
+print("Title: ", yt.title, "\nView: ", yt.views)
 
 print("Sound only? Yes - write whatever. False - click enter")
 soundOnly = input()
 
-stream = yt.streams.filter(only_audio=soundOnly).first().download('/Users/krusz/OneDrive/Pulpit/MusicFromYT')
+print("Set resolution: 144p, 240p, 360p, 480p, 720p, 1080p")
+resolution = input()
+
+stream = yt.streams.filter(only_audio = soundOnly, res = resolution).first().download('/Users/krusz/OneDrive/Pulpit/MusicFromYT')
