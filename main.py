facb78aa6350e42f220e69e084598d831f734e81
Bartosz Kruszyński <62513514+Bartosz383@users.noreply.github.com>
2022-11-06 11:31:52 +0100

    Open new window

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ from tkinter import filedialog, messagebox
 from pytube import YouTube
 from tkinter import *
 
+
 def WidgetsOneFile(root, videoLink, downloadPath, varCheckbox, clicked, resOptions):
     ytLink = Label(root, text="YouTube URL: ", width=13, bg="#33b249")
     ytLink.grid(row=0, column=0, padx=5, pady=5)
 
-    root.ytLinkText = Entry(root,bg="#a1d99b", width=60, textvariable=videoLink)
+    root.ytLinkText = Entry(root, bg="#a1d99b", width=60, textvariable=videoLink)
     root.ytLinkText.grid(row=0, column=1, padx=5, pady=5)
 
     destinationLabel = Label(root, text="Destination: ", width=13, bg="#33b249")
@@ -15,10 +16,11 @@ def WidgetsOneFile(root, videoLink, downloadPath, varCheckbox, clicked, resOptio
     root.destinationText = Entry(root, width=60, bg="#a1d99b", textvariable=downloadPath)
     root.destinationText.grid(row=1, column=1, padx=5, pady=5)
 
-    browseButton = Button(root, text="Browse", command=lambda : Browse(downloadPath), width=13, bg="#33b249")
+    browseButton = Button(root, text="Browse", command=lambda: Browse(downloadPath), width=13, bg="#33b249")
     browseButton.grid(row=1, column=2, padx=5, pady=5)
 
-    soundCheckbox = Checkbutton(root, text="Sound only", width=10, bg="#33b249", variable=varCheckbox, onvalue=True, offvalue=False)
+    soundCheckbox = Checkbutton(root, text="Sound only", width=10, bg="#33b249", variable=varCheckbox, onvalue=True,
+                                offvalue=False)
     soundCheckbox.deselect()
     soundCheckbox.grid(row=0, column=2, padx=5, pady=5)
 
@@ -30,29 +32,34 @@ def WidgetsOneFile(root, videoLink, downloadPath, varCheckbox, clicked, resOptio
     resolutionMenu["menu"].config(bg="#33b249")
     resolutionMenu.grid(row=3, column=0, padx=5, pady=5)
 
-    downloadButton = Button(root, text="Download", command=lambda : DownloadVideo(videoLink, downloadPath,varCheckbox, clicked), width=25, bg="#33b249")
+    downloadButton = Button(root, text="Download",
+                            command=lambda: DownloadVideo(videoLink, downloadPath, varCheckbox, clicked), width=25,
+                            bg="#33b249")
     downloadButton.grid(row=2, column=1, padx=5, pady=5)
 
     quitButton = Button(root, text="Quit", command=root.quit, width=25, bg="#33b249")
     quitButton.grid(row=3, column=1, padx=5, pady=5)
 
+
 def WidgetsStart(master, varCheckbox, clicked):
+    nextButton = Button(master, text="Next", command=MenuDownloadOneVideo, width=25, bg="#33b249")
+    nextButton.grid(row=0, column=1, padx=5, pady=5)
 
-
-
-    multipleFileCheckbox = Checkbutton(master, text="Multiple File", width=20, bg="#33b249", variable=varCheckbox, onvalue=True, offvalue=False)
+    multipleFileCheckbox = Checkbutton(master, text="Multiple File", width=20, bg="#33b249", variable=varCheckbox,
+                                       onvalue=True, offvalue=False)
     multipleFileCheckbox.deselect()
     multipleFileCheckbox.grid(row=1, column=1, padx=5, pady=5)
 
     quitButton = Button(master, text="Quit", command=master.quit, width=25, bg="#33b249")
     quitButton.grid(row=3, column=1, padx=5, pady=5)
 
+
 def Browse(downloadPath):
     downlandDirectory = filedialog.askdirectory(initialdir="Your Directory Path")
     downloadPath.set(downlandDirectory)
 
-def DownloadVideo(videoLink, downloadPath,varCheckbox, clicked):
 
+def DownloadVideo(videoLink, downloadPath, varCheckbox, clicked):
     url = videoLink.get()
     folder = downloadPath.get()
     onlyAudioSet = varCheckbox.get()
@@ -65,21 +72,22 @@ def DownloadVideo(videoLink, downloadPath,varCheckbox, clicked):
         getStream = getVideo.streams.filter(only_audio=onlyAudioSet, res=resolutio).first()
     getStream.download(folder)
 
-    messagebox.showinfo("Download Successful", "Your video is here " + getVideo.title + "\nVideo length: " + str(getVideo.length) + " seconds" + "\n" + folder)
+    messagebox.showinfo("Download Successful", "Your video is here " + getVideo.title + "\nVideo length: " + str(
+        getVideo.length) + " seconds" + "\n" + folder)
 
-def DownloadMultipleVideo():
 
+def MenuDownloadMultipleVideo():
     path = "C:\\Users\\krusz\\Music\\"
     manyURLs = open("D:\Repozytoria i inne takie\yt_downloader\links.txt", 'r')
     count = 1
 
     for i in manyURLs:
         try:
-            yt=YouTube(i)
+            yt = YouTube(i)
         except:
             print("Error")
 
-        yt.streams.filter(progressive = True, file_extension='mp4')
+        yt.streams.filter(progressive=True, file_extension='mp4')
         hr = yt.streams.get_highest_resolution()
 
         try:
@@ -91,7 +99,8 @@ def DownloadMultipleVideo():
 
     print("Completed")
 
-def DownloadOneVideo():
+
+def MenuDownloadOneVideo():
     root = Tk()
     root.title("YouTube Downloader")
     root.iconbitmap('YTdownloaderIcon.ico')
@@ -108,9 +117,10 @@ def DownloadOneVideo():
     WidgetsOneFile(root, videoLink, downloadPath, varCheckbox, clicked, resOptions)
 
     root.mainloop()
+
 def main():
     master = Tk()
-    #master.geometry("300x300")
+    # master.geometry("300x300")
     master.title("YouTube Downloader")
     master.iconbitmap('YTdownloaderIcon.ico')
     master.configure(bg="black")
@@ -120,32 +130,11 @@ def main():
 
     WidgetsStart(master, varCheckbox, clicked)
 
-    def openNewWindow():
-        # Toplevel object which will
-        # be treated as a new window
-        newWindow = Toplevel(master)
-
-        # sets the title of the
-        # Toplevel widget
-        newWindow.title("New Window")
-
-        # sets the geometry of toplevel
-        newWindow.geometry("200x200")
-
-        # A Label widget to show in toplevel
-        Label(newWindow,
-              text="This is a new window").pack()
-
-    nextButton = Button(master, text="Next", command=openNewWindow, width=25, bg="#33b249")
-    nextButton.grid(row=0, column=1, padx=5, pady=5)
-
-
     master.mainloop()
 
 
-
 if __name__ == "__main__":
-    main()
-    #DownloadOneVideo()
-    #DownloadMultipleVideo()
-    #master()
+    #main()
+    MenuDownloadOneVideo()
+    # DownloadMultipleVideo()
+    # master()
